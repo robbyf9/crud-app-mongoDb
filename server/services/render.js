4d379beec623b003cfb293ff1dfb32d2c2691e8e
robbyf9 <febriansyahrobby9@gmail.com>
2021-12-08 16:34:26 +0700

    refactoring code

--- server/services/render.js
+++ server/services/render.js
@@ -17,8 +17,6 @@ exports.add_user = (req,res) => {
 exports.update_user = (req, res) => {
     axios.get(`http://localhost:3000/api/users/`, {params: {id: req.query.id}})
         .then(function(response){
-            // console.log(req)
-            // res.render(req)
             res.render('update_user', {users: response.data});
         })
         .catch(err => {
