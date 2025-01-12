bec3ff130dda6d220fcf815ceeccfd0cc8af8edb
robbyf9 <febriansyahrobby9@gmail.com>
2025-01-12 14:47:11 +0700

    CRUD Users Mongo DB

--- server/model/model.js
+++ server/model/model.js
@@ -1,20 +1,22 @@
-const mongoose = require('mongoose');
+const { Int32 } = require("mongodb");
+const mongoose = require("mongoose");
 
 let scheme = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    gender:String,
-    status:String
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  umur: {
+    type: Number,
+    required: true,
+  },
+});
 
-})
+const userDb = mongoose.model("userdb", scheme);
 
-const userDb = mongoose.model('userdb', scheme);
-
-module.exports = userDb
+module.exports = userDb;
--- server/services/render.js
+++ server/services/render.js
@@ -1,25 +1,27 @@
-const axios = require('axios');
+const axios = require("axios");
 
-exports.homeRoutes = (req,res) => {
-    axios.get('http://localhost:3000/api/users')
-        .then(function(response){
-            res.render('index', {users: response.data});
-        })
-        .catch(err => {
-            res.status(500).send('Oops.. Something wrong happened.')
-        })
-}
+exports.homeRoutes = (req, res) => {
+  axios
+    .get("http://localhost:8080/api/users")
+    .then(function (response) {
+      res.render("index", { users: response.data });
+    })
+    .catch((err) => {
+      res.status(500).send("Oops.. Something wrong happened.");
+    });
+};
 
-exports.add_user = (req,res) => {
-    res.render('add_user');
-}
+exports.add_user = (req, res) => {
+  res.render("add_user");
+};
 
 exports.update_user = (req, res) => {
-    axios.get(`http://localhost:3000/api/users/`, {params: {id: req.query.id}})
-        .then(function(response){
-            res.render('update_user', {users: response.data});
-        })
-        .catch(err => {
-            res.status(500).send('Oops.. Something wrong happened.')
-        })
-}
+  axios
+    .get(`http://localhost:8080/api/users/`, { params: { id: req.query.id } })
+    .then(function (response) {
+      res.render("update_user", { users: response.data });
+    })
+    .catch((err) => {
+      res.status(500).send("Oops.. Something wrong happened.");
+    });
+};
